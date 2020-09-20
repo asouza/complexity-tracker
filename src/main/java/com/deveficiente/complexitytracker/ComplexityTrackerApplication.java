c3000c5fd615e6885798daad3b4890206eb965c8
alberto souza <alots.ssa@gmail.com>
2020-09-20 00:02:54 -0300

    using snapshot version of ck

--- src/main/java/com/deveficiente/complexitytracker/ComplexityTrackerApplication.java
+++ src/main/java/com/deveficiente/complexitytracker/ComplexityTrackerApplication.java
@@ -1,16 +1,23 @@
 package com.deveficiente.complexitytracker;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.repodriller.RepoDriller;
 import org.repodriller.RepositoryMining;
 import org.repodriller.domain.Commit;
 import org.repodriller.filter.range.Commits;
 import org.repodriller.persistence.NoPersistence;
 import org.repodriller.persistence.PersistenceMechanism;
-import org.repodriller.persistence.csv.CSVFile;
 import org.repodriller.scm.CommitVisitor;
 import org.repodriller.scm.GitRepository;
 import org.repodriller.scm.SCMRepository;
+import org.repodriller.util.RDFileUtils;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.tmatesoft.sqljet.core.internal.lang.SqlParser.delete_stmt_return;
+
+import com.github.mauricioaniche.ck.CK;
 
 @SpringBootApplication
 public class ComplexityTrackerApplication {
@@ -29,14 +36,15 @@ public class ComplexityTrackerApplication {
 		 */
 
 		new RepoDriller().start(() -> {
-			new RepositoryMining().in(GitRepository.singleProject(
-					"/Users/albertoluizsouza/ambiente/projetos/spring-framework"))
+			new RepositoryMining()
+					.in(GitRepository.singleProject("/Users/albertoluizsouza/ambiente/projetos/SSP"))
 					// ultimo commit da tag v3.0.0.M1
 					.through(Commits
-							.single("dfcbc6f659b20d4f30929122ed3d2e28aa76360f"))
+							.single("f74be96dada91d6d15cc7c3954050e4133de16bf"))
 					.process(new DevelopersVisitor(), new NoPersistence())
 					.mine();
 		});
+		
 	}
 
 	static class DevelopersVisitor implements CommitVisitor {
@@ -46,9 +54,17 @@ public class ComplexityTrackerApplication {
 				PersistenceMechanism writer) {
 
 			System.out.println("===============process ===================");
-			System.out.println(repo.getPath());
-			System.out.println(commit.getAuthor());
+			System.out.println(repo.getPath()
+					+ "/src/main/java/org/jasig/ssp/service/impl");
+			String folderToInspect = repo.getPath()
+					+ "/src/main/java/org/jasig/ssp/service/impl";
 
+			CK ck = new CK(false, 0, false);
+			ck.calculate(folderToInspect, result -> {
+				System.out.println("Classe :" + result.getClassName());
+				System.out.println("LOC :" + result.getLoc());
+			});
+			
 		}
 	}
 
