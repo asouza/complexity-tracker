0140b62028bf11de75dbaa4939b98e3d97671e03
alberto souza <alots.ssa@gmail.com>
2020-09-21 07:33:33 -0300

    more ideas to report

--- src/main/java/com/deveficiente/complexitytracker/reports/ReportController.java
+++ src/main/java/com/deveficiente/complexitytracker/reports/ReportController.java
@@ -2,10 +2,14 @@ package com.deveficiente.complexitytracker.reports;
 
 public class ReportController {
 
-	/*  
-	 * 	- Graficos de linha
-	 *  - gerar relatório de loc agrupado por classe entre commits
+	/*  	
+	 * - Gráfico de barra
+	 *  - gerar relatório de métricas agrupado por classe entre commits
+	 *  
+	 *  - Graficos de linha
 	 *  - gerar relatório de soma de loc do grupo analisado entre commits
-	 *  - gerar relatório de média de loc do grupo analisado entre commits
+	 *  - gerar relatório de média de métrica do grupo analisado entre commits
+	 *  - gerar relatório combinando soma de métricas do grupo analisado
+	 *  - gerar relatório de média de combinação de métrica do grupo analisado entre commits
 	 */
 }
