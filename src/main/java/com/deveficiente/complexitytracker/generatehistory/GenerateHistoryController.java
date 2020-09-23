a9aec8ce3af01d577df0f54681e030bf1ce354f3
alberto souza <alots.ssa@gmail.com>
2020-09-23 07:39:57 -0300

    import generate 201 to first report

--- src/main/java/com/deveficiente/complexitytracker/generatehistory/GenerateHistoryController.java
+++ src/main/java/com/deveficiente/complexitytracker/generatehistory/GenerateHistoryController.java
@@ -1,5 +1,6 @@
 package com.deveficiente.complexitytracker.generatehistory;
 
+import java.net.URI;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -14,10 +15,13 @@ import org.repodriller.scm.GitRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Controller
 public class GenerateHistoryController {
@@ -31,31 +35,43 @@ public class GenerateHistoryController {
 			.getLogger(GenerateHistoryController.class);
 
 	@PostMapping(value = "/generate-history")
-	public String generate(@Valid GenerateHistoryRequest request) {
+	// 1
+	@ResponseBody
+	public ResponseEntity<?> generate(@Valid GenerateHistoryRequest request,
+			UriComponentsBuilder uriComponent) {
 
 		List<String> hashes = request.parseCommitsHashes();
 		log.debug("Let's generate history for {} commits {} ", hashes.size(),
 				hashes);
 
+		// 2
 		InMemoryComplexityHistoryWriter inMemoryWriter = new InMemoryComplexityHistoryWriter();
+		// 3
 		new RepoDriller().start(() -> {
 			new RepositoryMining()
 					.in(GitRepository.singleProject(request.getLocalGitPath()))
 					.through(Commits.list(hashes))
+					// 4
 					.process(new HistoryVisitor(request.getProjectId()),
 							inMemoryWriter)
 					.mine();
 		});
 
+		// 5
 		tx.executeWithoutResult(status -> {
 			manager.createQuery(
 					"delete from ComplexityHistory c where c.projectId = :projectId")
 					.setParameter("projectId", request.getProjectId())
 					.executeUpdate();
+			// 6
 			inMemoryWriter.getHistory().forEach(manager::persist);
 		});
 
-		return "redirect:/...";
+		URI complexityHistoryGroupedReportUri = uriComponent.path(
+				"/reports/pages/complexity-by-class?projectId={projectId}")
+				.buildAndExpand(request.getProjectId()).toUri();
+		return ResponseEntity.created(complexityHistoryGroupedReportUri)
+				.build();
 
 	}
 
